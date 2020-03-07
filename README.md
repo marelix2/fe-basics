d4b7b2c1829960e1250ddacccb1274d2edf2fcb3
Michal <marelix2@gmail.com>
2020-03-07 20:08:07 +0100

    ex1: tasks complete

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-Projekt z zadaniami na prezentacje
+Celem tego zadania jest zainicjowanie repo z wykożystaniem npm oraz wykonanie pierwszych skryptow.
+
+tl;dr;
+
+npm init npm run script_name
---
+++ ex2.js
@@ -0,0 +1,27 @@
+const intNumber = 11
+const floatNumber = 10.01
+
+console.log("Wynik dodawania float i int: ",intNumber + floatNumber)
+
+const hello = 'Hello'
+const there = "There"
+const general = `General Kenobi`
+
+console.log(`well ${hello + ' ' + there}`, general)
+
+const nullValue = null
+const undefinedValue = undefined
+
+console.log('wynik dodawania nulla i undefined: ', nullValue + undefinedValue)
+
+const NaNValue = general / intNumber
+console.log('NaN: ', NaNValue)
+
+const obj = {}
+const table = []
+
+console.log('wynik dodawania obiektu z obiektem :', {} + {})
+console.log('wynik dodawania obiektu z tablica :', {} + [])
+console.log('wynik dodawania tablicy z obiektem :', [] + {})
+
+
